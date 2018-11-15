8d9cde87c712904c1236a14956a6caa7264a564e
Lucas Fernando Correa de Souza <lucasfernandocorreadesouza@MacBook-Pro-de-lfcsvt.local>
2018-11-15 15:02:23 +0100

    first commit

---
+++ house.js
@@ -0,0 +1,182 @@
+// it is the array where our data is stored
+//var senData = data.results[0].members
+// gets  the rows will be placed oon the html file
+var tblBody = document.getElementById("putTheRowsHere")
+// gets the element where the table is in the html file
+var tbl = document.getElementById("myTable");
+// create the row for out table
+var row = document.createElement("tr");
+// this function creates a table with all of the objects in the data array using only the selected values of each object
+function createOriginalTable(senData) {
+    // this clears the table body everytime we start a function   
+    tblBody.innerHTML = ""
+    for (var i = 0; i < senData.length; i++) {
+
+        if (senData[i].middle_name === null) {
+            senData[i].middle_name = " "
+        }
+
+        var fullName = senData[i].first_name + ' ' + senData[i].middle_name + ' ' + senData[i].last_name
+        var row = document.createElement("tr");
+
+        row.insertCell().innerHTML = fullName.link(senData[i].url)
+        row.insertCell().innerHTML = senData[i].party
+        row.insertCell().innerHTML = senData[i].state
+        row.insertCell().innerHTML = senData[i].seniority
+        row.insertCell().innerHTML = senData[i].votes_with_party_pct + "%"
+
+        tblBody.appendChild(row);
+
+    }
+    tbl.appendChild(tblBody);
+    tbl.setAttribute("class", "table table-hover");
+}
+
+
+
+
+//this function populate the states in the seletc state box
+function createSelect(senData) {
+
+    //create array with state values extracted from the data to populate the states in the seletc state box
+    var stateArray = []
+    var newArray = []
+    for (var i = 0; i < senData.length; i++) {
+
+        stateArray.push(states = senData[i].state)
+        stateArray.forEach(function (item) {
+            if (newArray.indexOf(item) < 0) {
+                newArray.push(item);
+            }
+        });
+        newArray.sort()
+
+    }
+
+    //this code feeds the     
+    var select = document.getElementById("selectState");
+
+    for (var i = 0; i < newArray.length; i++) {
+        var opt = newArray[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        select.appendChild(el);
+    }
+}
+
+// this function creates the filter for the table by party and state
+function checkMyCh(senData) {
+    // this varuiable determines the value the user choose in the select state box    
+    var stateValue = document.getElementById("selectState").value;
+    // this variables returns the value chosen when the checkbox is checked in the form of an array and it returns a inpute value to be a condition for creting the table
+    var checkedCheckbox = Array.from(document.querySelectorAll('input[name=parties]:checked')).map(function (oneInput) {
+        return oneInput.value
+    });
+    // this condition creates a table when neither checkboxes or state select are activated(returns all unfiltered data)
+    if (checkedCheckbox.length === 0 && stateValue === "all") {
+        createOriginalTable(senData)
+        //this condition will create a filtered by party as the state is not selected   
+    } else if (checkedCheckbox.length > 0 && stateValue === "all") {
+        tblBody.innerHTML = ""
+        for (var i = 0; i < senData.length; i++) {
+            if (checkedCheckbox.includes(senData[i].party)) {
+                createFilteredTable(senData[i])
+            }
+        }
+        // this condition creates a table if objects that filtering only by state.(checkbos is not checked and state is one state)        
+    } else if (checkedCheckbox.length === 0 && stateValue !== "all") {
+        tblBody.innerHTML = ""
+        for (var i = 0; i < senData.length; i++) {
+            if (stateValue === senData[i].state) {
+                createFilteredTable(senData[i])
+            }
+        }
+
+        // this creates a table filtered by party and state take both values inputed and create a table by party and state
+    } else {
+        tblBody.innerHTML = ""
+        for (var i = 0; i < senData.length; i++) {
+            if (checkedCheckbox.includes(senData[i].party) && stateValue === senData[i].state) {
+
+                createFilteredTable(senData[i])
+            }
+        }
+    }
+
+}
+
+function createFilteredTable(senator) {
+
+    if (senator.middle_name === null) {
+        senator.middle_name = " "
+    }
+
+    var fullName = senator.first_name + ' ' + senator.middle_name + ' ' + senator.last_name
+    var row = document.createElement("tr");
+
+    row.insertCell().innerHTML = fullName.link(senator.url)
+    row.insertCell().innerHTML = senator.party
+    row.insertCell().innerHTML = senator.state
+    row.insertCell().innerHTML = senator.seniority
+    row.insertCell().innerHTML = senator.votes_with_party_pct + "%"
+
+    tblBody.appendChild(row);
+}
+
+function getData() {
+    fetch('https://api.propublica.org/congress/v1/113/house/members.json', {
+            headers: new Headers({
+                'X-API-Key': 'XiS3U2dJVOswHAgjUcMNIBkG4aANYLQIBCowZeNB'
+            })
+        })
+        .then(response => response.json())
+        .then(response => {
+            const senData = response.results[0].members
+            createOriginalTable(senData),
+                createSelect(senData),
+                activateEventListeners(senData)
+
+
+
+            console.log(senData)
+        })
+
+
+        .catch(err => console.log(err));
+}
+
+window.onload = () => getData()
+
+function activateEventListeners(senData) {
+    Array.from(document.querySelectorAll('input[name="parties"]'))
+        .forEach(input => input.addEventListener('change', () => checkMyCh(senData)))
+
+    document.querySelector('#selectState').addEventListener('change', () => checkMyCh(senData))
+}
+
+//
+//var senData = undefined
+//    fetch('https://api.propublica.org/congress/v1/113/senate/members.json', {
+//            headers: new Headers({
+//                'X-API-Key': 'XiS3U2dJVOswHAgjUcMNIBkG4aANYLQIBCowZeNB'
+//            })
+//        })
+//        .then(response => response.json())
+//        .then(response => {
+//         senData = response.results[0].members
+//         createOriginalTable(response.results[0].members),
+//             createSelect(response.results[0].members)
+////         checkMyCh(response.results[0].members)
+//
+//        
+//        console.log(senData)
+//    })
+//    
+//        
+//        .catch(err => console.log(err));
+
+
+
+//getData('https://api.propublica.org/congress/v1/113/senate/members.json')
+//getData('https://api.propublica.org/congress/v1/113/house/members.json')
---
+++ loyalty.js
@@ -0,0 +1,276 @@
+function getData() {
+    fetch('https://api.propublica.org/congress/v1/113/senate/members.json', {
+            headers: new Headers({
+                'X-API-Key': 'XiS3U2dJVOswHAgjUcMNIBkG4aANYLQIBCowZeNB'
+            })
+        })
+        .then(response => response.json())
+        .then(response => {
+            const senData = response.results[0].members
+            //            createOriginalTable(senData),
+            //                createSelect(senData),
+            //                activateEventListeners(senData)
+//            leastLoyal(senData)
+//                leastLoyalMembers(senData)
+//                percentage(senData)
+//                mostLoyal(senData)
+//                mostLoyaMembers(senData)
+                main(senData)
+        })
+
+        .catch(err => console.log(err));
+}
+
+function main(senData){
+    mostLoyal(senData)
+    leastLoyal(senData)
+    leastLoyaltable(senData)
+    mostLoyaltable(senData)
+    numbersTable(senData)
+    statistics.numOfDemocrats = count(senData, "D")
+    statistics.numOfRepublicans = count(senData, "R")
+    statistics.numOfIndependents = count(senData, "I")
+    statistics.numberOfMembers = count(senData, "I") + count(senData, "D") + count(senData, "R")
+    statistics.percentOfDemocrats = percentVoted(senData, "D")
+    statistics.percentOfRepublicans = percentVoted(senData, "R")
+    statistics.percentOfIndependents = percentVoted(senData, "I")
+    statistics.percentOfMembers = totalPercentage(senData).toFixed(2)
+    statistics.allOfMostLoyal = mostLoyalMembers(senData)
+    statistics.allOfLeastLoyal = leastLoyalMembers(senData)
+    statistics.allOfMostEngaged = mostEngagedMember(senData)
+    statistics.allOfLeastEngaged = leastEngagedMember(senData)
+    
+   
+    console.log(statistics)
+   
+}
+
+var statistics = { 
+ numOfDemocrats: 0,
+ numOfRepublicans: 0,
+ numOfIndependents: 0,
+ numberOfMembers: 0,
+ percentOfDemocrats: 0,
+ percentOfRepublicans: 0,
+ percentOfIndependents: 0,
+ percentOfMembers: 0,
+ allOfMostLoyal: 0,
+ allOfLeastLoyal: 0,
+allOfMostEngaged: 0,
+allOfLeastEngaged: 0
+}
+
+window.onload = () => getData()
+// count the mumber of member per each party, takes one argument partyCode, in this case "D", "R" or "I"
+function count(senData, partyCode) {
+    return senData
+        .filter(senator => senator.party === partyCode)
+        .length
+}
+
+// calculate the percentage the members voted with the party, takes one argument partyCode, in this case "D", "R" or "I"
+function percentVoted(senData, partyCode) {
+    var memberspercent = []
+    var members = senData.filter(senator => senator.party === partyCode)
+    for (var i = 0; i < members.length; i++) {
+        memberspercent.push(senData[i].votes_with_party_pct)
+
+    }
+
+    // this sums up all the values and the the result is divided by the number of members to create an average  
+    memberspercent = memberspercent.reduce((a, b) => a + b, 0) / members.length
+    memberspercent = memberspercent.toFixed(2);
+    return memberspercent
+}
+// returns the lowest votes missed percentage of the chosen percentage, pass by the pecentage(percent , with one parameter passed), returning a minimum to be compared in the leastEngagedMember().
+function mostEngaged(senData) {
+    var allMissed = []
+    senData.forEach(member => {
+        allMissed.push(member.missed_votes_pct)
+        allMissed = allMissed.sort((a, b) => a - b).slice(0, 10);
+
+    })
+    
+    return Math.max(...allMissed)
+    //    return allMissed
+}
+// returns the highest votes missed percentage of the chosen percentage of congressmen, pass by the pecentage(percent , with one parameter passed)
+function leastEngaged(senData) {
+    var leastMissed = []
+    senData.forEach(member => {
+        leastMissed.push(member.missed_votes_pct)
+        leastMissed = leastMissed.sort((a, b) => b - a).slice(0, 10 );
+    })
+
+    return Math.min(...leastMissed)
+}
+
+// filters most engaged member by getting the member whon mossed votes percentage is lower that set at mostEngaged()
+function mostEngagedMember(senData) {
+    return senData.filter(member => member.missed_votes_pct <= mostEngaged(senData))
+
+}
+// filters most engaged member by getting the member whon mossed votes percentage is lower that set at leastEngaged()
+function leastEngagedMember(senData) {
+    
+    return senData.filter(member => member.missed_votes_pct >= leastEngaged(senData))
+
+}
+
+// returns the highest votes with party percentage of the chosen percentage of congressmen, pass by the pecentage(percent , with one parameter passed)
+function mostLoyal(senData) {
+    var loyalUp = []
+    senData.forEach(member => { 
+        loyalUp.push(member.votes_with_party_pct)
+        loyalUp = loyalUp.sort((a, b) => b - a).slice(0, senData.length / 100 * 10)
+//                                      
+    })
+    return Math.min(...loyalUp)
+}
+
+// returns the lowest votes with party percentage of the chosen percentage of congressmen, pass by the pecentage(percent , with one parameter passed)
+function leastLoyal(senData) {
+    var loyalDown = []
+    senData.forEach(member => {
+        loyalDown.push(member.votes_with_party_pct)
+        loyalDown = loyalDown.sort((a, b) => a - b).slice(0, senData.length / 100 * 10);
+
+    })
+   
+    return Math.max(...loyalDown)
+    //        return loyalDown 
+}
+// returns an array where the vote with party percentage of the member are higer than the min determined in the mostLoyal()
+function mostLoyalMembers(senData) {
+    return senData.filter(member => member.votes_with_party_pct >= mostLoyal(senData))
+
+}
+
+// returns an array where the vote with party percentage of the member are higer than the min determined in the leastLoyal()
+function leastLoyalMembers(senData) {
+//    console.log(senData.filter(member => member.votes_with_party_pct <= leastLoyal(senData))) 
+    return senData.filter(member => member.votes_with_party_pct <= leastLoyal(senData))
+
+}
+// create a function to calculate the total of votes percentage
+function totalPercentage(senData) {
+    var membersPercentTotal = []
+    for (var i = 0; i < senData.length; i++) {
+        membersPercentTotal.push(senData[i].votes_with_party_pct)
+        var result = membersPercentTotal.reduce(function (a, b) {
+            return a + b;
+        }, 0);
+    }
+    return result / senData.length
+}
+
+// creates table with the leastLoyal member displaying full name(as a link), total number of party votes(rounded not to show the remainder) and percentage of votes with the party
+function leastLoyaltable(senData) {
+    var table = document.getElementById('leastLoyal')
+    var tBody = document.getElementById('leastLoyalBody')
+    var newArr = statistics.allOfLeastLoyal = leastLoyalMembers(senData)
+    var fullName = senData.map(member => member.first_name + " " + member.middle_name + " " + member.last_name)
+    
+    for (var i = 0; i < newArr.length; i++) {
+
+        if (newArr[i].middle_name === null) {
+            newArr[i].middle_name = " "
+        }
+
+        var partyVotes = newArr[i].total_votes / 100 * newArr[i].votes_with_party_pct
+        partyVotes = Math.round(partyVotes);
+
+        var fullName = newArr[i].first_name + ' ' + newArr[i].middle_name + ' ' + newArr[i].last_name
+        var row = document.createElement("tr");
+
+        row.insertCell().innerHTML = fullName.link(newArr[i].url)
+        row.insertCell().innerHTML = partyVotes
+        row.insertCell().innerHTML = newArr[i].votes_with_party_pct
+
+
+        tBody.appendChild(row);
+
+
+        table.appendChild(tBody);
+    }
+    table.setAttribute("class", "table table-hover")
+}
+
+// creates table with the mostLoyal member displaying full name(as a link), total number of party votes(rounded not to show the remainder) and percentage of votes with the party
+function mostLoyaltable(senData) {
+    var table = document.getElementById('mostLoyal')
+    var tBody = document.getElementById('mostLoyalBody')
+    var fullName = senData.map(member => member.first_name + " " + member.middle_name + " " + member.last_name)
+    var newArr = statistics.allOfMostLoyal = mostLoyalMembers(senData)
+
+    for (var i = 0; i < newArr.length; i++) {
+
+        if (newArr[i].middle_name === null) {
+            newArr[i].middle_name = " "
+        }
+
+        var partyVotes = newArr[i].total_votes / 100 * newArr[i].votes_with_party_pct
+        partyVotes = Math.round(partyVotes);
+
+        var fullName = newArr[i].first_name + ' ' + newArr[i].middle_name + ' ' + newArr[i].last_name
+        var row = document.createElement("tr");
+
+        row.insertCell().innerHTML = fullName.link(newArr[i].url)
+        row.insertCell().innerHTML = partyVotes
+        row.insertCell().innerHTML = newArr[i].votes_with_party_pct
+
+
+        tBody.appendChild(row);
+
+
+        table.appendChild(tBody);
+    }
+
+    table.setAttribute("class", "table table-hover")
+}
+
+// creates the table with the number of congresmen per party and a total
+function numbersTable(senData) {
+    var table = document.getElementById("totalMembers")
+    var tBody = document.getElementById("totalMembersBody")
+    var newArray = cretaArray(senData)
+
+    for (var i = 0; i < newArray.length; i++) {
+        var row = document.createElement("tr");
+        row.insertCell().innerHTML = newArray[i].name
+        row.insertCell().innerHTML = newArray[i].numberOfMembers
+        row.insertCell().innerHTML = newArray[i].votesPercentage
+
+        tBody.appendChild(row);
+    }
+    table.appendChild(tBody);
+    table.setAttribute("class", "table table-hover")
+
+}
+
+
+
+// create an array of objects called statistics
+
+function cretaArray(senData){
+    var array =  [{
+        name: statistics.nameOfDemocrats = 'Democrats',
+        numberOfMembers: statistics.numOfDemocrats = count(senData, "D"),
+        votesPercentage: statistics.percentOfDemocrats = percentVoted(senData, "D")},
+                  {
+        name: statistics.nameOfRepublicans = 'Republicans',
+        numberOfMembers: statistics.numOfDemocrats = count(senData, "R"),
+        votesPercentage: statistics.percentOfDemocrats = percentVoted(senData, "R")},
+                  {
+        name: statistics.nameOfIndependents = 'Independents',
+        numberOfMembers: statistics.numOfDemocrats = count(senData, "I"),
+        votesPercentage: statistics.percentOfDemocrats = percentVoted(senData, "I")},
+                  {
+         name: statistics.nameOfTotal = "Total",
+        numberOfMembers: statistics.numberOfMembers = count(senData, "I") + count(senData, "D") + count(senData, "R"),
+        votesPercentage: statistics.percentOfMembers = totalPercentage(senData).toFixed(2)}
+                 ]
+        console.log(array)
+    return array
+    
+}
